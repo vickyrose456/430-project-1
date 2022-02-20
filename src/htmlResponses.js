d7d92164cfad49590d1ae2039628884176e3437e
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-02-19 21:14:09 -0500

    using api hw 2 as baseline

---
+++ src/htmlResponses.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+// helper fns
+const serveFile = (response, file, contentType) => {
+  response.writeHead(200, { 'Content-Type': contentType });
+  response.write(file);
+  response.end();
+};
+
+// Serve the client.html page
+const getIndex = (request, response) => {
+  serveFile(response, index, 'text/html');
+};
+
+// Serve the style.css page
+const getCSS = (request, response) => {
+  serveFile(response, css, 'text/css');
+};
+
+
+module.exports = {
+  getIndex,
+  getCSS,
+};
---
+++ src/server.js
@@ -0,0 +1,75 @@
+const http = require('http');
+
+const url = require('url');
+
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+}; // end parseBody
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // If they go to /addUser
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};// end handlePost
+
+const urlStruct = {
+
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notFound,
+    '/addUser': jsonHandler.addUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notFoundMeta,
+    '/addUser': jsonHandler.addUser,
+    notFound: jsonHandler.notFound,
+  },
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
